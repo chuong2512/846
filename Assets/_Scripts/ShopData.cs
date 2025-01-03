ed3a252fb9c237a39636f4b323557a549ecccc08
pro_c <hieutg244@gmail.com>
2025-01-03 22:21:17 +0700

    up

--- Assets/_Scripts/ShopData.cs
+++ Assets/_Scripts/ShopData.cs
@@ -27,42 +27,6 @@ public class ShopData : MonoBehaviour
     {
         var url = packJsonLink.Trim();
         if (string.IsNullOrEmpty(url)) return;
-
-        StartCoroutine(GetTextFromServer(url));
     }
-
-    private IEnumerator GetTextFromServer(string url)
-    {
-        WWW www = new WWW(url);
-        yield return www;
-
-        if (www.isDone && string.IsNullOrEmpty(www.error))
-        {
-            PackDatas data = null;
-            try
-            {
-                data = JsonUtility.FromJson<PackDatas>(www.text);
-            }catch(Exception e)
-            {
-                Debug.Log(e.Message);
-            }
-
-            if (data == null || data.packDatas.Count == 0)
-            {
-                Debug.LogWarning("Json content is empty or incorrect format");
-                yield break;
-            }
-
-            packList = data.packDatas;
-        }
-        else
-        {
-            if (numTry == 1)
-                Debug.LogError("Can't get json content from server");
-
-            numTry++;
-            yield return new WaitForSeconds(0.3f);
-            StartCoroutine(GetTextFromServer(url));
-        }
-    }
+    
 }
